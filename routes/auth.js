26e5783bb836a623dbcb279092d7d0bed41d6adc
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-03-31 00:22:08 +0200

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get( '/me',protect,getMe)
-
 router.put( '/updatedetails',protect,updateDetails)
 router.put( '/updatepassword',protect,updatePassword)
 
