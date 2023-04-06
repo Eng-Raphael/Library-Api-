d24e79cbbf6d6189410c6633c7ea45f41418ce4d
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-04-06 10:42:24 +0200

    create author function update

--- controllers/author.js
+++ controllers/author.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
@@ -31,17 +33,18 @@ exports.getAuthor = asyncHandler(async (req, res, next) => {
 // @desc Create an author
 // @route POST /api/authors
 // @access Private (Admin)
+// Set up file storage configuration
 exports.createAuthor = [
   body('firstName').isLength({ min: 3, max: 20 }).exists().withMessage('First name is required'),
   body('lastName').isLength({ min: 3, max: 20 }).exists().withMessage('Last name is required'),
   body('dob').exists().withMessage('Date of birth is required'),
 
-  // eslint-disable-next-line consistent-return
   async (req, res, next) => {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        const errorArray = errors.array().map((error) => error.msg);
+        return res.status(400).json({ errors: errorArray });
       }
 
       const author = new Author({
@@ -51,29 +54,25 @@ exports.createAuthor = [
       });
 
       if (!req.files) {
-        return next(new ErrorResponse('Please upload a file', 404));
+        return res.status(400).json({ errors: ['Please upload a file'] });
       }
 
       const file = req.files.image;
 
       if (!file.mimetype.startsWith('image')) {
-        return next(new ErrorResponse('Please upload an image file', 404));
+        return res.status(400).json({ errors: ['Please upload an image file'] });
       }
 
       if (file.size > process.env.MAX_FILE_UPLOAD) {
-        return next(
-          new ErrorResponse(
-            `Please upload image file less than ${process.env.MAX_FILE_UPLOAD}`,
-            404,
-          ),
-        );
+        return res.status(400).json({
+          errors: [`Please upload image file less than ${process.env.MAX_FILE_UPLOAD}`],
+        });
       }
 
       file.name = `photo_profile_${Date.now()}${path.parse(file.name).ext}`;
 
       file.mv(
         `${process.env.FILE_UPLOAD_PATH}/authors/${file.name}`,
-        // eslint-disable-next-line consistent-return
         async (err) => {
           if (err) {
             console.error(err);
@@ -94,7 +93,6 @@ exports.createAuthor = [
       next(err);
     }
   },
-
 ];
 
 // @desc Update an author
