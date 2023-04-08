ff496487de55b9af9716571d274395064b5c5ed0
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-04-08 16:09:44 +0200

    User book controllers function update and validation

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const {
   addBookToUser,
   updateBookToUser,
+  deleteBookForUser,
 } = require('../controllers/user');
 
 const { protect } = require('../middleware/auth');
@@ -12,5 +13,6 @@ const { protect } = require('../middleware/auth');
 router
   .route('/:id/book')
   .post(protect, addBookToUser)
-  .put(protect, updateBookToUser);
+  .put(protect, updateBookToUser)
+  .delete(protect, deleteBookForUser);
 module.exports = router;
