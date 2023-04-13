5c06e97b993e930aab40bba43b0a6daf80af0d13
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-04-13 17:10:29 +0200

    atlas

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: 'libraryApi',
   });
   console.log(`Mongo db connected ${conn.connection.host}`.cyan.underline.bold);
 };
