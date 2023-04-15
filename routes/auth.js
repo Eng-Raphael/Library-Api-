e82a1551b365a84249eb8770df4bd0e2499e65a2
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-04-15 16:20:39 +0200

    fixed one error

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,5 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/me', protect, getMe);
-router.put('/updatedetails', protect, updateDetails);
-router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
